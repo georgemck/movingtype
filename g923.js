0254c607bfcf3555b3d8fcc349cfc4e52d54a466
GeorgeMcK <george@hookstream.com>
2024-05-02 09:23:05 -0700

    Create Logitech g923.js

---
+++ g923.js
@@ -0,0 +1,42 @@
+// Logitech G923 keycodes
+// Generative AI is experimental.
+// Learn more… Here are the keycodes for the Logitech G923 racing wheel in JavaScript:
+
+const G923_KEYCODE = {
+  // Buttons
+  BUTTON_A: 0,
+  BUTTON_B: 1,
+  BUTTON_X: 2,
+  BUTTON_Y: 3,
+  BUTTON_LB: 4,
+  BUTTON_RB: 5,
+  BUTTON_SELECT: 6,
+  BUTTON_START: 7,
+  BUTTON_L3: 8,
+  BUTTON_R3: 9,
+  BUTTON_UP: 10,
+  BUTTON_DOWN: 11,
+  BUTTON_LEFT: 12,
+  BUTTON_RIGHT: 13,
+  BUTTON_GUIDE: 14,
+
+  // D-Pad
+  DPAD_UP: 15,
+  DPAD_DOWN: 16,
+  DPAD_LEFT: 17,
+  DPAD_RIGHT: 18,
+
+  // Paddles
+  PADDLE_LEFT: 19,
+  PADDLE_RIGHT: 20,
+
+  // Shifter
+  SHIFTER_UP: 21,
+  SHIFTER_DOWN: 22,
+
+  // Axis
+  AXIS_STEERING: 0,
+  AXIS_CLUTCH: 1,
+  AXIS_GAS: 2,
+  AXIS_BRAKE: 3,
+};
